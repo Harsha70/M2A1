40db802865a23aa9b5a021b5df3179c4fa43da06
Harshavardhan Reddy Manyam (RVP) <hmanyam@INT.DFM.com>
2026-01-07 12:17:27 -0600

    'postman_doc'

--- src/middleware/tierCheck.ts
+++ src/middleware/tierCheck.ts
@@ -5,7 +5,7 @@ import prisma from '../db';
 export const tierCheck = async(req: Request, res: Response, next: NextFunction): Promise<any> => {
     const user = req.user;
     if (!user) return res.status(401).json({ error: "Unauthorized" });
-    console.log(user)
+    // console.log(user)
     if(user.tier !== 'enterprise') {
         return res.status(403).json({ 
             error: "Bulk creation is an Enterprise feature. Please upgrade your plan." 
