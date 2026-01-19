10b4ae7f27e61be1c981a7fc2e9eafb2876349a2
Harsha70 <manyamharsha112@gmail.com>
2026-01-18 22:49:59 -0600

    M2A7

--- dist/src/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const adapter_better_sqlite3_1 = require("@prisma/adapter-better-sqlite3");
-const client_1 = require("@prisma/client");
-const adapter = new adapter_better_sqlite3_1.PrismaBetterSqlite3({
-    url: "file:./dev.db"
-});
-const prisma = new client_1.PrismaClient({ adapter });
-exports.default = prisma;
